eb4089be4c3e785dd874f77ca5842e9934cc77c2
sidhuraj <mr.prvsantosh@rediffmail.com>
2020-04-05 19:30:31 +0530

    first commit

---
+++ CollectionviewEx/CollectionviewEx.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 5a3648b4ef5e13a1d2d118cd95234f86b651b80a */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1284
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 336409
+	.include	"typemap.jm.inc"
---
+++ CollectionviewEx/CollectionviewEx/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionviewEx
+{
+    public class State
+    {
+        public string StatePhoto { set; get; }
+
+        public string StateName { set; get; }
+    }
+}
